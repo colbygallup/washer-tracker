9ff7d181b43e15e13bff807224626951f2d6f07e
Colby Gallup <colbygallup@gmail.com>
2020-12-04 14:26:18 -0600

    Finished callback setup

--- pi_client.py
+++ pi_client.py
@@ -12,4 +12,6 @@ def on_vibrate(channel):
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect()
+GPIO.add_event_detect(10, GPIO.RISING, callback=on_vibrate)
+
+input('Press enter to quit\n\n')
