020008ae736baf609f162548170b010eb48b5f10
Colby Gallup <colbygallup@gmail.com>
2021-01-05 18:17:16 -0600

    Abstracted vibration sensor code

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-from collections import defaultdict
-from typing import Dict
-import time
+from flask.json import jsonify
+from sensors.vibration_sensor import VibrationSensor
+from sensor import Sensor
 
-from flask import Flask, request
+from flask import Flask
 
 
 app: Flask = Flask(__name__)
-washers: Dict[int, int] = defaultdict(lambda: False)
 
-
-@app.route('/update', methods=['POST'])
-def update() -> str:
-    data: Dict = request.get_json()
-    try:
-        washers[int(data['washer_id'])] = time.time()  # seconds since epoch
-        return 'success'
-    except ValueError:
-        return 'fail'
+sensor: Sensor = VibrationSensor()
+sensor.register(app)
 
 
 @app.route('/', methods=['GET'])
 def index() -> str:
-    out: str = "<ul>"
-    for key, value in washers.items():
-        out += "<li>{}: {}</li>".format(key, time.time() - value <= 30)
-    out += "</ul>"
-    return out
+    return 'test'
+
+
+@app.route('/washers', methods=['GET'])
+def washers() -> str:
+    return jsonify({i: sensor.get_washer(i).name for i in range(10)})
